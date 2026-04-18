6bf699c182bb18d35de7c30cb3180a562c03d35d
Mash <mashunya112006@gmail.com>
2026-04-18 17:04:15 +0300

    Delete Lab1/IDrawStrategy.cs

--- Lab1/IDrawStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyShape
-{
-    public interface IDrawStrategy
-    {
-        void Draw(Graphics graphics, Shape shape); // Method to draw a shape on the graphics surface
-        Rectangle GetBounds(Shape shape); // Calculates the bounding rectangle of the shape
-        bool ContainsPoint(Shape shape, int pointX, int pointY); // Checks if the given coordinates are inside the shape's boundaries
-    }
-}
